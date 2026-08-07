1bda7de470c120bb2699f067deb6d75fd33b7b49
Poss-Rexim <ddt1472582022@outlook.com>
2026-08-07 16:09:51 +0800

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require('core/basic')
-require('core/keybind')
-require('plugins/module')
---require('vim._core.ui2').enable({
---    enable = true
---})
-require('core/lsp')
-vim.cmd.colorscheme("everforest")
